3416f380e75d26b65cd48d219b62dc63f8ad2606
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-14 18:23:26 -0500

    added navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,39 @@
+<script>
+	// TODO: Move logo to appropriate directory.
+	import metrobotsLogo from '$lib/assets/gdb.png';
+</script>
+
+<nav>
+	<img id="metrobots-logo" src={metrobotsLogo} alt="Metrobots Logo" />
+	<div id="page-links">
+		<p>About Us</p>
+		<p>Our Season</p>
+		<p>Our Community</p>
+		<p>Resources</p>
+	</div>
+</nav>
+
+<style>
+	nav {
+		background-color: lightblue;
+		min-height: 10vw;
+		padding: 0 2vw;
+
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	#metrobots-logo {
+		width: 10vw;
+	}
+
+	#page-links {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+
+		width: 50%;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,24 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
+	import metrobotsAppleTouchIcon from '$lib/assets/favicon/apple-touch-icon.png';
+	import metrobots32x32Logo from '$lib/assets/favicon/favicon-32x32.png';
+	import metrobots16x16Logo from '$lib/assets/favicon/favicon-16x16.png';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" sizes="180x180" href={metrobotsAppleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="32x32" href={metrobots32x32Logo} />
+	<link rel="icon" type="image/png" sizes="16x16" href={metrobots16x16Logo} />
 </svelte:head>
 
-{@render children()}
+<Navbar />
+
+<main>
+	{@render children()}
+</main>
+
+<Footer />
