8a90dbf68b63319acc4b48315f9459f27f01dbdf
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-16 11:22:27 -0500

    added webmanifest to +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import metrobotsAppleTouchIcon from '$lib/assets/favicon/apple-touch-icon.png';
 	import metrobots32x32Logo from '$lib/assets/favicon/favicon-32x32.png';
 	import metrobots16x16Logo from '$lib/assets/favicon/favicon-16x16.png';
+	import manifest from '$lib/assets/favicon/site.webmanifest';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '../app.css';
@@ -13,6 +14,7 @@
 	<link rel="apple-touch-icon" sizes="180x180" href={metrobotsAppleTouchIcon} />
 	<link rel="icon" type="image/png" sizes="32x32" href={metrobots32x32Logo} />
 	<link rel="icon" type="image/png" sizes="16x16" href={metrobots16x16Logo} />
+	<link rel="manifest" href={manifest} />
 </svelte:head>
 
 <Navbar />
