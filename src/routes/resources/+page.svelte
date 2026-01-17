6983f91d3600951e8166cd0f8dbdf310160cfd4e
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-17 11:10:03 -0500

    added swervesim

---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<main>
+	<div id="links-container">
+		<h1>Team 3324 Resources</h1>
+		<a href={resolve('/resources/swervesim')}>Swerve Simulator</a>
+		<a href={resolve('/resources/scouting')}>Scouting App</a>
+	</div>
+</main>
