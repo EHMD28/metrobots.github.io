1e1de484c6a426379b97d57e5acbec3fdbc6c6b6
EHMD28 <ehmdiagne0530@gmail.com>
2026-01-28 19:03:16 -0500

    added important dates section

--- src/routes/season/2025-2026/+page.svelte
+++ src/routes/season/2025-2026/+page.svelte
@@ -6,50 +6,65 @@
 </svelte:head>
 
 <main>
-	<h1>2025-2026 Season</h1>
-	<h2>Game and Strategy</h2>
-	<iframe
-		width="560"
-		height="315"
-		src="https://www.youtube.com/embed/_fybREErgyM?si=kySmouyowCehhqzy"
-		title="YouTube video player"
-		frameborder="0"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-		referrerpolicy="strict-origin-when-cross-origin"
-		allowfullscreen
-	></iframe>
+	<section>
+		<h1>2025-2026 Season</h1>
+		<h2>Game and Strategy</h2>
+		<iframe
+			width="560"
+			height="315"
+			src="https://www.youtube.com/embed/_fybREErgyM?si=kySmouyowCehhqzy"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+		></iframe>
+		<br />
+		<p>
+			This section can give a brief overview of what this year's game is and our strategies for
+			tackling it.
+		</p>
+		<br />
+		<h2>Important Dates</h2>
+		<ul id="important-dates">
+			<li><b>Kickoff:</b> January 10th, 2026.</li>
+			<li><b>Stop Build Day:</b> February 21st, 2026.</li>
+			<li><b>Regional Competitons:</b> March 19-21 and April 2-4.</li>
+			<li><b>World Championship:</b> April 18-21, 2026 in Houston Texas.</li>
+		</ul>
+	</section>
 	<br />
-	<p>
-		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta laborum repellendus numquam
-		excepturi praesentium alias quas quod at libero magni, amet ipsa odit laboriosam ut esse
-		deserunt quibusdam error voluptates.
-	</p>
-	<br />
-	<h2>Robot Design</h2>
-	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptates molestiae alias
-		pariatur facilis ad beatae, voluptate officiis repudiandae vel numquam, nesciunt neque quos
-		consequuntur similique architecto amet. Nisi, fugit.
-	</p>
-	<br />
-	<a href="">2025-2026 Robot CAD (Onshape) - NOT LINKED YET</a>
-	<a href="https://github.com/metrobots/26-final">2025-2026 Robot Code</a>
+	<section>
+		<h2>Robot Design</h2>
+		<p>
+			This section can give an overview of our robot design: a list of our subsystems and an
+			overview of how they work (shooter, storage, climb, etc.). Also, we should have a to link the
+			CAD document for the full robot, or at least the subsystems.
+		</p>
+		<br />
+		<a href="" target="_blank">2025-2026 Robot CAD (Onshape) - NOT LINKED YET</a>
+		<a href="https://github.com/metrobots/26-final" target="_blank">2025-2026 Robot Code</a>
+	</section>
 </main>
 
 <style>
 	main {
 		padding: 5vh 5vw;
+	}
 
+	section {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
 
-	h1 {
+	h1,
+	h2 {
 		text-align: center;
 	}
 
-	a {
+	a,
+	p {
 		align-self: baseline;
 	}
 </style>
